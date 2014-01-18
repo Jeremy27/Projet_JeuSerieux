44294819600e22953bb9d848beb26c2f86c7c651
jeremy27 <courel.jeremy@gmail.com>
2014-01-18 03:15:15 +0100

    PanelInformations OK

--- SimulationCapitaineriePort/src/view/PanelInformations.java
+++ SimulationCapitaineriePort/src/view/PanelInformations.java
@@ -7,45 +7,62 @@
 package view;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.util.ArrayList;
-import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
  *
- * @author jeremy
+ * @author Jérémy
  */
 public class PanelInformations extends JPanel {
     
     private JLabel              _jlNomPanel;
     private ArrayList<String>   _informations;
-    private GridLayout          _layout;
 
+    /**
+     * 
+     * @param nomPanel
+     * @param informations format de l'ArrayList : {"nom : valeur", "nom : valeur", ...}
+     */
     public PanelInformations(String nomPanel, ArrayList<String> informations) {
-        this._jlNomPanel    = new JLabel(nomPanel);
-        this._informations  = informations;
-        this._layout        = new GridLayout(informations.size()+1, 1, 5, 5);
+        _jlNomPanel    = new JLabel(nomPanel, JLabel.CENTER);
+        _informations  = informations;
         
-        this.setLayout(_layout);
-        this._jlNomPanel.setBorder(BorderFactory.createLineBorder(Color.red, 10));
+        setLayout(new GridLayout(informations.size()+1, 1));
+        _jlNomPanel.setOpaque(true);
+        _jlNomPanel.setBackground(Color.black);
+        _jlNomPanel.setForeground(Color.white);
+        _jlNomPanel.setPreferredSize(new Dimension(this.getWidth(), 50));
+        _jlNomPanel.setAlignmentX((float) 10.0);
+        _jlNomPanel.setFont(new Font(this.getFont().getName(), Font.BOLD, 18));
     }
     
     public void majInformations() {
+        JLabel jl;
+        boolean pair = true;
         add(_jlNomPanel);
-        for(String info : _informations)
-            add(new JLabel(info));
+        for (String info : _informations) {
+            jl = new JLabel(info);
+            jl.setOpaque(true);
+            if(pair = !pair)
+                jl.setBackground(Color.lightGray);
+            else
+                jl.setBackground(Color.gray);
+            add(jl);
+        }
     }
     
     public static void main(String[] args) {
         JFrame jf = new JFrame();
         ArrayList<String> informations = new ArrayList<>();
         
-        for (int i = 0; i < 10; i++) {
-            informations.add("Test : "+i);
-        }
+        for (int i = 0; i < 10; i++)
+            informations.add("  Test : " + i + "  ");
         
         PanelInformations pi = new PanelInformations("TEST", informations);
         pi.majInformations();
@@ -54,7 +71,6 @@ public class PanelInformations extends JPanel {
         jf.pack();
         jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
     }
     
 }
