40c3282be7d42df1d257d970bc0ca90f36834d99
Gary Hébert <hebertgary.76@gmail.com>
2014-02-20 09:17:02 +0100

    mise à jour

--- SimulationCapitaineriePort/src/main/java/jeuserieux/presentation/IHM.java
+++ SimulationCapitaineriePort/src/main/java/jeuserieux/presentation/IHM.java
@@ -1,19 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package jeuserieux.presentation;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
 import java.awt.geom.Point2D;
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
